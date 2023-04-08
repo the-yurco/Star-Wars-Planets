5163fb411d13ed525d8797248bb1e463f998f1a3
georgee <jurajbosy1@gmail.com>
2023-04-08 23:19:03 +0200

    first setup of vue-app

--- src/App.vue
+++ src/App.vue
@@ -1,6 +1,5 @@
 <template>
-  <img alt="Vue logo" src="./assets/logo.png">
-  <HelloWorld msg="Welcome to Your Vue.js + TypeScript App"/>
+	<div class="app">hello world</div>
 </template>
 
 <script lang="ts">
@@ -8,20 +7,9 @@ import { defineComponent } from 'vue';
 import HelloWorld from './components/HelloWorld.vue';
 
 export default defineComponent({
-  name: 'App',
-  components: {
-    HelloWorld
-  }
+	name: 'App',
+	components: {}
 });
 </script>
 
-<style lang="scss">
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
-</style>
+<style lang="scss"></style>
--- src/main.ts
+++ src/main.ts
@@ -1,5 +1,6 @@
-import { createApp } from 'vue'
-import App from './App.vue'
-import store from './store'
+import { createApp } from 'vue';
+import App from './App.vue';
+import store from './store';
+import './main.css';
 
-createApp(App).use(store).mount('#app')
+createApp(App).use(store).mount('#app');
