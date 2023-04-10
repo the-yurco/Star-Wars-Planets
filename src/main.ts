20378f357f6aad96f6fbebf34533808d8eecc992
georgee <jurajbosy1@gmail.com>
2023-04-10 23:02:22 +0200

    updated version of setup

--- src/main.ts
+++ src/main.ts
@@ -1,6 +1,14 @@
-import { createApp } from 'vue';
-import App from './App.vue';
-import store from './store';
-import './main.css';
+import { createApp } from 'vue'
+import { createPinia } from 'pinia'
 
-createApp(App).use(store).mount('#app');
+import App from './App.vue'
+import router from './router'
+
+import './assets/main.css'
+
+const app = createApp(App)
+
+app.use(createPinia())
+app.use(router)
+
+app.mount('#app')
