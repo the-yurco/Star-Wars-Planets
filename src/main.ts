d1eed025c62fe38aae18b8e08805cae6a2596091
georgee <jurajbosy1@gmail.com>
2023-04-13 22:02:31 +0200

    reset of my app

--- src/main.ts
+++ src/main.ts
@@ -1,14 +1,5 @@
 import { createApp } from 'vue'
-import { createPinia } from 'pinia'
-
 import App from './App.vue'
-import router from './router'
+import store from './store'
 
-import './assets/main.css'
-
-const app = createApp(App)
-
-app.use(createPinia())
-app.use(router)
-
-app.mount('#app')
+createApp(App).use(store).mount('#app')
