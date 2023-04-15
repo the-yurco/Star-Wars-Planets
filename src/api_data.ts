6355c42a44f080777a8b3921617e24052d2519bf
georgee <jurajbosy1@gmail.com>
2023-04-15 21:17:12 +0200

    feat: Configure API data fetching + configured interfaces
    In this commit, I configured the API data fetching process and interface configurations

--- src/api_data.ts
+++ src/api_data.ts
@@ -1,2 +1,39 @@
-// Constant that holds our base URL
+//? Planet Interface:
+export interface PlanetInfo {
+	name: string;
+	rotation_period: string;
+	orbital_period: string;
+	diameter: string;
+	climate: string;
+	gravity: string;
+	terrain: string;
+	surface_water: string;
+	population: string;
+	residents: string[];
+}
+
+//? interface to match an object of properties in Planet int. make into array
+interface PlanetResponse {
+	results: PlanetInfo[];
+}
+
+// //! Constant that holds our base URL
 const API_URL = 'https://swapi.py4e.com/api';
+
+//! FETCH function => fetchPlanets
+// This function is an asynchronous function that takes an optional searchTerm parameter, which is a string
+// The function returns a Promise of an array of objects that match the PlanetInfo interface
+export async function fetchPlanets(searchTerm = ''): Promise<PlanetInfo[]> {
+	//---------------------------------------------------------------------
+
+	// FETCH function for making request to SWAPI api
+	// SEARCH-TERM = search variable for our planets
+	const response = await fetch(`${API_URL}/planets/?search=${searchTerm}`);
+
+	// The RESPONSE from the API is converted to JSON
+	// DATA => resulting JSON
+	const data: PlanetResponse = await response.json();
+
+	// DATA RESULTS => returned from the function
+	return data.results;
+}
