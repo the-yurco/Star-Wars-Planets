b40aa299f78ef28fdc3ae02f78de399122cb1cc9
georgee <jurajbosy1@gmail.com>
2023-04-16 13:36:27 +0200

    feat: Add search functionality and reactive data to Star Wars Planets app
    - I added a text input element that allows users to search for planets by name in the Star Wars Planets application.
    - I used the v-model directive to bind the searchTerm to the value of the search input element, making it reactive and easy to use.
    - Additionally, I defined a setup function that stores the user's search term and the list of planets fetched from the API as reactive properties using the ref function.

--- src/App.vue
+++ src/App.vue
@@ -1,27 +1,54 @@
 <template>
-	<h1>STAR WARS PLANETS</h1>
+	<div id="app">
+		<div class="container">
+			<h1 class="header-main-txt text-center my-5">
+				Star Wars <span>Planets</span>
+			</h1>
+
+			<!--? A text input element that allows users to search for planets -->
+			<!--! v-model ==> it is directive used to bind the searchTerm to the value of the search input ele. -->
+			<div class="input-group mb-3">
+				<div class="input-group-prepen">
+					<span
+						class="input-group-text rounded-0 rounded-start"
+						id="basic-addon1"
+					>
+						<img src="./assets/colorized-icon-logo.png" alt="" />
+					</span>
+				</div>
+				<input
+					type="text"
+					v-model="searchTerm"
+					class="form-control text-white"
+					placeholder="Planet's name"
+				/>
+			</div>
+		</div>
+	</div>
 </template>
 
 <script lang="ts">
-	import { defineComponent } from 'vue';
+	// IMPORTS
+	import { defineComponent, ref } from 'vue';
+	import PlanetCardComponent from './components/PlanetCardComponent.vue';
+	import { fetchPlanets, PlanetInfo } from './api_data';
 
 	export default defineComponent({
 		name: 'App',
-		components: {}
+
+		components: {},
+
+		// A SETUP function that defines the reactive data and functions that are used in the component
+		setup() {
+			const searchTerm = ref(''); // property that sores the user's search term
+			const planets = ref<PlanetInfo[]>([]); // property that sores the list of planets fetched from API
+
+			return {
+				searchTerm,
+				planets
+			};
+		}
 	});
 </script>
 
-<style lang="scss">
-	// just basic setup for our app
-
-	body {
-		$bg-color: #0d151f;
-		$txt-color: #fff;
-		$font-family: sans-serif;
-
-		color: $txt-color;
-		background: $bg-color;
-		font-family: $font-family;
-		text-align: center;
-	}
-</style>
+<style></style>
