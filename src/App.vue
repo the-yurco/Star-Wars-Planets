3d865f8bdd1a41023f6415ae10599e9e1f3c5c23
georgee <jurajbosy1@gmail.com>
2023-04-16 18:33:20 +0200

    feat: Finished main functionality of CardComponent and AppComponent - searching is working - planets are not showing when you enter into the site - and it write error message when you enter the site - so need to make only modal window then finally I need to fix this remaining bugs

--- src/App.vue
+++ src/App.vue
@@ -20,8 +20,42 @@
 					type="text"
 					v-model="searchTerm"
 					class="form-control text-white"
+					@input="handleSearchInput"
 					placeholder="Planet's name"
 				/>
+			</div>
+			<div class="wrapper mt-5">
+				<!--? A loading message that is displayed when the application is fetching data from the API  -->
+				<div v-if="isLoading">
+					<button class="btn btn-dark text-center" type="button" disabled>
+						<span
+							class="spinner-border spinner-border-sm"
+							role="status"
+							aria-hidden="true"
+						></span>
+						<!-- <span class="visually-hidden text-white">Loading...</span> -->
+					</button>
+				</div>
+
+				<!--? A message that is displayed when no planets are found  -->
+				<!--! v-else ==> it is a directive used to print msg when no planets were found -->
+				<div v-else-if="planets.length === 0" class="text-white">
+					<div
+						class="alert bg-danger text-black d-flex align-items-center w-25"
+						role="alert"
+					>
+						<div>No Planets found</div>
+					</div>
+				</div>
+
+				<!--? A list of planet cards that are rendered for each planet found in the API  -->
+				<div v-else class="row">
+					<PlanetCardComponent
+						v-for="planet in planets"
+						:key="planet.name"
+						:planet="planet"
+					/>
+				</div>
 			</div>
 		</div>
 	</div>
@@ -36,16 +70,29 @@
 	export default defineComponent({
 		name: 'App',
 
-		components: {},
+		components: {
+			PlanetCardComponent
+		},
 
 		// A SETUP function that defines the reactive data and functions that are used in the component
 		setup() {
 			const searchTerm = ref(''); // property that sores the user's search term
 			const planets = ref<PlanetInfo[]>([]); // property that sores the list of planets fetched from API
+			const isLoading = ref(false); // property that stores if the app is currently fetching data from API
+
+			// An zsync function that is called when user types in the search input
+			// it fetches the list of planets that match the seaerxh term and updates the planets property
+			const handleSearchInput = async () => {
+				isLoading.value = true;
+				planets.value = await fetchPlanets(searchTerm.value);
+				isLoading.value = false;
+			};
 
 			return {
 				searchTerm,
-				planets
+				planets,
+				isLoading,
+				handleSearchInput
 			};
 		}
 	});
--- src/components/PlanetCardComponent.vue
+++ src/components/PlanetCardComponent.vue
@@ -1,14 +1,34 @@
 <template>
-	<div>
-		<h1>CARD</h1>
+	<div class="col-md-3 col-sm-6 mb-4">
+		<div class="card p-3 rounded">
+			<!--  <h2> and a <p> element to display the name and population of the planet  -->
+			<h3>{{ planet.name }}</h3>
+			<p>
+				Population:
+				{{ planet.population }}
+			</p>
+		</div>
 	</div>
+	<!-- ! The values of these elements are obtained from the planet prop -->
 </template>
 
 <script lang="ts">
-	import { defineComponent } from 'vue';
+	// The script imports defineComponent and PropType from the vue lib. , as well as the PlanetInfo interface from an API_DATA file
+	import { defineComponent, PropType } from 'vue';
+	import { PlanetInfo } from '../api_data';
 
 	export default defineComponent({
-		name: 'PlanetCard'
+		// component's name
+		name: 'PlanetCard',
+
+		// props object contains a PLANET prop
+		// PLANET prop is defimed as an object with a PropType set to PlanetInfo + required oprion set to TRUE
+		props: {
+			planet: {
+				type: Object as PropType<PlanetInfo>,
+				required: true
+			}
+		}
 	});
 </script>
 
