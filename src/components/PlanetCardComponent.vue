76126b887b413f0af0ce196345d9dafbb29d0c27
georgee <jurajbosy1@gmail.com>
2023-04-25 20:09:49 +0200

    feat: SEO features

--- src/components/PlanetCardComponent.vue
+++ src/components/PlanetCardComponent.vue
@@ -1,7 +1,7 @@
 <template>
 	<div class="col-xl-3 col-sm-6 mb-4">
 		<div class="card p-3 rounded" @click="handleClick">
-			<!--  <h2> and a <p> element to display the name and population of the planet  -->
+			<!--  <h3> and a <p> element to display the name and population of the planet  -->
 			<h3>{{ planet.name }}</h3>
 			<p>
 				Population:
